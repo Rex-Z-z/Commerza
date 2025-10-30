1c4b5fbf9add35e113e31e25d411e86f2dd26917
Rex <195429265+Rex-Z-z@users.noreply.github.com>
2025-10-30 09:26:41 +0700

    Navigation Menu

---
+++ components/ui/list-item.tsx
@@ -0,0 +1,25 @@
+import React from "react"
+import Link from "next/link"
+import { NavigationMenuLink } from "@/components/ui/navigation-menu"
+
+function ListItem({
+  title,
+  children,
+  href,
+  ...props
+}: React.ComponentPropsWithoutRef<"li"> & { href: string }) {
+  return (
+    <li {...props}>
+      <NavigationMenuLink asChild>
+        <Link href={href}>
+          <div className="text-sm leading-none font-medium">{title}</div>
+          <p className="text-muted-foreground line-clamp-2 text-sm leading-snug">
+            {children}
+          </p>
+        </Link>
+      </NavigationMenuLink>
+    </li>
+  )
+}
+
+export default ListItem
